8121e010b2c0f80412016cfed028142abc2e9099
amixam <robertobrinko@gmail.com>
2024-01-05 16:10:18 +0200

    vercel compatability test

--- frontend/src/Pages/Gallery/index.js
+++ frontend/src/Pages/Gallery/index.js
@@ -2,29 +2,30 @@ import React, { useEffect, useState } from "react";
 import "./index.scss";
 
 function GalleryPage() {
-    const [images, setImages] = useState([]);
+    // const [images, setImages] = useState([]);
 
-    useEffect(() => {
-        const getImages = async () => {
-            try {
-                const response = await fetch("http://127.0.0.1:8000/images");
-                if (!response.ok) {
-                    throw new Error("Network response was not ok.");
-                }
+    // useEffect(() => {
+    //     const getImages = async () => {
+    //         try {
+    //             const response = await fetch("http://127.0.0.1:8000/images");
+    //             if (!response.ok) {
+    //                 throw new Error("Network response was not ok.");
+    //             }
 
-                const data = await response.json();
-                setImages(data.images);
-            } catch (error) {
-                console.error("Error fetching images: ", error);
-            }
-        };
+    //             const data = await response.json();
+    //             setImages(data.images);
+    //         } catch (error) {
+    //             console.error("Error fetching images: ", error);
+    //         }
+    //     };
 
-        getImages();
-    }, []);
+    //     getImages();
+    // }, []);
 
     return (
         <>
-            <div className="image-container">
+            <p>not for vercel yet..</p>
+            {/* <div className="image-container">
                 {images.map((image, index) => (
                     <div key={index}>
                         <img
@@ -36,7 +37,7 @@ function GalleryPage() {
                         <p>{image.picture_data.description}</p>
                     </div>
                 ))}
-            </div>
+            </div> */}
         </>
     );
 }
