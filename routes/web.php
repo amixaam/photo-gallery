3d10b04ed1ff5ef6f8fa69f40069a2ad3bb560e0
Amixaam <60570885+amixaam@users.noreply.github.com>
2024-05-10 19:56:47 +0300

    trusted proxies change

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use Inertia\Inertia;
 Route::get('/login', function () {
     return Inertia::render('Login');
 })->name('login');
+
 Route::get('/register', function () {
     return Inertia::render('Register');
 })->name('register');
