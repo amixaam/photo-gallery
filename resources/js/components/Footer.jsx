86b0eafeb96b265f5900210345eee8827ca707a4
Amixaam <60570885+amixaam@users.noreply.github.com>
2024-06-13 00:00:40 +0300

    Update Footer.jsx

--- resources/js/components/Footer.jsx
+++ resources/js/components/Footer.jsx
@@ -8,31 +8,26 @@ export const Footer = ({ grainPrefs, setGrainPrefs }) => {
                 <div className="wave-footer" />
             </div>
             <div className="relative h-fit bg-gradient-to-t from-footer to-footersecondary">
-                <div className="grid grid-cols-[1fr_1fr] gap-10 p-app-small opacity-75 sm:p-app">
-                    <div className="flex flex-col gap-2 sm:gap-6">
+                <div className="grid grid-cols-[1fr_1fr] gap-16 p-app-small opacity-75 sm:p-app">
+                    <div className="flex flex-col gap-6">
                         <h4 className="text-text">About</h4>
                         <Link href="https://github.com/amixaam/photo-gallery">
                             Source code
                         </Link>
-                        <Link>Figma design</Link>
-                        <Link href="https://www.linkedin.com/in/robertsbrinkis/">
-                            LinkedIn
+                        <Link href="https://www.figma.com/design/pL5zrSYfIO0uTQIQxT4XJf/Photo-portfolio?node-id=457-229&t=C08T1sJH4cPv9xPc-1">
+                            Design
                         </Link>
-                        <Link>CV</Link>
                     </div>
-                    <div className="flex flex-col flex-wrap gap-3 sm:gap-6">
+                    <div className="flex flex-col flex-wrap gap-6">
                         <h4 className="text-text">Socials</h4>
                         <Link href="https://www.instagram.com/robisnis/">
                             Instagram
                         </Link>
                         <Link href="https://twitter.com/amixaaam">X</Link>
-                        <Link href="https://www.youtube.com/@amixam">
-                            YouTube
-                        </Link>
                     </div>
 
-                    <div className="col-span-2 flex flex-row justify-between gap-3">
-                        <p className="link text-text">
+                    <div className="col-span-2 flex flex-row justify-between gap-3 items-end">
+                        <p className="link text-text text-xs sm:text-base">
                             © Roberts Briņķis, 2024
                         </p>
                         <div className="flex flex-row gap-8">
